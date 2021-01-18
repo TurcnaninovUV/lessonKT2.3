bd79b67ab5ce5892e46c72f31891c4f3e3625cd6
TurcnaninovUV <pro-h92@yandex.ru>
2021-01-18 17:59:45 +0300

    Исправил замечания

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    var likes = 121
-    if (likes % 10 == 1 && likes != 11)
+    var likes = 534
+    if (likes % 10 == 1 && likes % 100 != 11 && likes != 11)
         println("Понравилось $likes человеку")
     else if (likes == 1)
         println("Понравилось $likes человеку")
